9cf6b93828bf157e60e0974b4c4fd853f2f83f7e
HarikaKirlampalli <kirlampalliharika@gmail.com>
2026-07-15 21:42:34 +0530

    Add files via upload
    Day 20 – Object-Oriented Programming (OOPs)
    📚 Topics Covered
    Introduction to OOPs
    Advantages of OOPs
    Classes
    Objects
    Attributes
    Methods

---
+++ python notes(OOPS)  codegnan day-20.py
@@ -0,0 +1,73 @@
+'''
+   OOPS
+  -------
+>>Object-oriented programming system(OOPS), this will be organizes the code using classes and objects..
+
+USES OF OOPS
+-------------
+>>code reusability
+>>Easy maintainance
+>>Clear understanding
+>>Better security
+
+**CLASS
+--------
+>>Class is a blueprint or a template used to create an object..
+
+class batch_4:
+   pass
+
+OBJECT
+------
+>>Object is a instance of the class
+
+@code
+class student:# class classname
+    studn = 'Harika'
+st_ = student()#st_ is an object
+print(st_)
+
+ATTRIBUTES
+-----------
+>>Attributes are the variables that belongs to an object or the class
+
+class how:
+    def details(self,name,age):
+        self.name = name
+        self.name = age
+    
+s1 = how()
+s1.details('Harika',23)
+print(s1.name)
+
+Methods
+--------
+>>Methods are nothing but, the functions inside the class..
+
+class Calculator:
+    def add(self,num,num_2):
+        print(num + num_2)
+    def sub(self,num,num_2):
+        print(num - num_2)
+cal = Calculator()
+cal.add(45,6)
+cal.sub(45,6)
+
+'''
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
